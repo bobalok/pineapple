4c4c8f3ddea775066066bc8dd40ba2f75a821582
technocalorie <findabirr@gmail.com>
2022-09-25 18:13:21 +0600

    flowbite initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
@@ -12,30 +13,6 @@ module.exports = {
       },
     },
   },
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#631c96",
 
-          secondary: "#97f4a4",
-
-          accent: "#ce1c43",
-
-          neutral: "#182026",
-
-          "base-100": "#E3E5E8",
-
-          info: "#73B8D9",
-
-          success: "#16643C",
-
-          warning: "#FAD029",
-
-          error: "#E0486E",
-        },
-      },
-    ],
-  },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
